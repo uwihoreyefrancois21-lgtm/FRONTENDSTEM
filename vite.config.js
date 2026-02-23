da8eb42e65fc523373d9560d53a3fcc56bd9e245
uwihoreye465 <Uwihoreyefrancois12@gmail>
2026-02-23 22:13:07 +0200

    fix build and publish directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+//})
+
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ensure Vite builds to dist for Render
+  },
 })
+
